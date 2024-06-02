a3f7d1f3b348f21dd083d1eedf2b46e0c45119ee
Miguel Cabrera <miguelecabrerav@gmail.com>
2024-06-02 14:40:29 -0400

    milagro 1
    solucion final al problema de herencia de plantilla

--- login/urls.py
+++ login/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-
-from .views import Vregistro, cerrar_sesion, logear
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import Vregistro
 
 urlpatterns = [
     
     path("", Vregistro.as_view(), name='registro'),
-    path("cerrar_sesion", cerrar_sesion, name='cerrar_sesion'),
-    path("logear", logear, name='logear'),
-    
     
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- login/views.py
+++ login/views.py
@@ -12,45 +12,7 @@ class Vregistro(View):
     
     def get(self, request):
         form = UserCreationForm()
-        return render(request, 'registro/registro.html', {'form':form})
+        return render(request, 'registro/registro.html')
     
     def post(self, request):
-        
-        form=UserCreationForm(request.POST) #almaceno la informacion en la variable form
-        
-        if form.is_valid(): #verifico si la informacion es valida y actuo de la sig manera
-            
-            usuario = form.save() #guarda la informacion en la BBDD
-            
-            login(request, usuario) #logea directamente al usuario en su cuenta
-            
-            return redirect('Home') 
-        else:
-             
-            for msg in form.error_messages:                      #recorremos los errores ingresados
-                messages.error(request,form.error_messages[msg])    #mostramos los errores que se almacenaron
-                
-            return render(request, 'registro/registro.html', {'form':form})        
-            
-            
-def cerrar_sesion(request):
-    
-    logout(request)
-    
-    return redirect('Home')
-
-def logear(request):
-    if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST) #guardamos la informacion recibida en la variable
-        if form.is_valid(): 
-            nombre_usuario = form.cleaned_data.get('username') #cleaned_data.get() para obtener la informacion del recuadro X
-            contraseña = form.cleaned_data.get('password')
-            usuario = authenticate(username = nombre_usuario, password = contraseña)
-            if usuario is not None:
-                login(request, usuario)
-                return redirect('Home')       
-        else:
-            messages.error(request, "Informacion incorrecta")
-                  
-    form = AuthenticationForm()
-    return render(request, 'logear/logear.html', {'form':form})
+        pass
